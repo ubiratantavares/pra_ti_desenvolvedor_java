e5d1ccc5a1293d32927591106632e8e982ccfad5
= <=>
2023-04-28 23:05:00 -0300

    Atualkização das classes Imc e ImcNovo

--- projeto01_OO/src/imc/Imc.java
+++ projeto01_OO/src/imc/Imc.java
@@ -2,13 +2,38 @@ package imc;
 
 public class Imc {
 
-    public double peso;
-    public double altura;
-    public double imc;
+    private double peso;
+    private double altura;
+    private double imc;
 
     public Imc(double peso, double altura) {
         this.peso = peso;
         this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getImc() {
+        this.imc = this.peso / (this.altura * this.altura);
+        return this.imc;
+    }
+
+    public void setImc(double imc) {
+        this.imc = imc;
     }
 
     public void calcularImc() {
--- projeto01_OO/src/imc/ImcNovo.java
+++ projeto01_OO/src/imc/ImcNovo.java
@@ -8,13 +8,13 @@ public class ImcNovo extends Imc {
 
     public String classificacao() {
         this.calcularImc();
-        if (this.imc < 16) return "Magreza grau III";
-        if (this.imc < 17) return "Magreza grau II";
-        if (this.imc < 18.5) return "Magreza grau I";
-        if (this.imc < 25.0) return "Saudável";
-        if (this.imc < 30) return "Sobrepeso";
-        if (this.imc < 35) return "Obesidade moderada (grau I)";
-        if (this.imc < 40) return "Obesidade severa (grau II)";
+        if (this.getImc() < 16) return "Magreza grau III";
+        if (this.getImc() < 17) return "Magreza grau II";
+        if (this.getImc() < 18.5) return "Magreza grau I";
+        if (this.getImc() < 25.0) return "Saudável";
+        if (this.getImc() < 30) return "Sobrepeso";
+        if (this.getImc() < 35) return "Obesidade moderada (grau I)";
+        if (this.getImc() < 40) return "Obesidade severa (grau II)";
         return "Obesidade muito severa (grau III";
     }
 }
